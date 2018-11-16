afb87611c89560cf5639045e6e9d83ab62929b41
Adrian Zwiener <adrian.zwiener@uni-tuebingen.de>
2018-11-16 17:42:37 +0100

    Bug fixes in normalized update model

--- src/update/normalized_update_model.cpp
+++ src/update/normalized_update_model.cpp
@@ -20,20 +20,42 @@ public:
         cslibs_math_3d::Vector3d normal = state.getNormal(map);
         KDL::Vector n(normal(0), normal(1), normal(2));
         KDL::Vector p(pos(0), pos(1), pos(2));
-        KDL::Wrench w(n,KDL::Vector::Zero());
+        KDL::Wrench w(-n,KDL::Vector::Zero());
+//        ROS_INFO_STREAM("p: " << p.x() << ", " <<p.y() << ", " << p.z());
+//        ROS_INFO_STREAM("n: " << n.x() << ", " <<n.y() << ", " << n.z());
         KDL::Frame T(KDL::Rotation::Identity(),p);
         w = T * w;
         Eigen::VectorXd tau_p = model_.getExternalTorques(joint_state.position, frame_id, w);
-        tau_p.normalize();
+        std::size_t rows = tau_p.rows();
+        std::size_t n_torques = joint_state.effort.size();
+        std::size_t offset = 0;
+        std::size_t dim = std::min(rows, n_torques);
+        if(rows > n_torques){
+          Eigen ::VectorXd tau_pp = Eigen::VectorXd::Zero(dim);
+          for (std::size_t i = 0; i < dim; ++i){
+            tau_pp(i) = tau_p(i);
+          }
+          tau_p = tau_pp;
+        } else {
+          offset = n_torques - rows;
+        }
+//        std::cout <<" rows " << rows << " | offset " << offset << " | dim " << dim << " | nt " << n_torques << std::endl;
         Eigen::VectorXd tau_f;
-        cslibs_kdl::convert(joint_state.position, tau_f,  joint_state.position.size() - model_.getNrJoints());
+        cslibs_kdl::convert(joint_state.effort, tau_f, offset);
+//        ROS_INFO_STREAM("particle torque: " << tau_p << " sensed torque: " << tau_f);
+        tau_p.normalize();
         tau_f.normalize();
         Eigen::VectorXd diff = tau_f - tau_p;
+//        ROS_INFO_STREAM("difference: "<< diff);
 
-        double expo = (diff.transpose()).eval() * info_matrix_ * diff;
-        return std::exp(-0.5*expo);
+        double expo = (diff.transpose()).eval() * info_matrix_.block(0,0,dim,dim) * diff;
+        double result = std::exp(-0.5*expo);
+//        ROS_INFO_STREAM("weight of particle: " << result << " exponent: " << expo);
+//        if(result < 0.2){
+//          ROS_INFO_STREAM("small weight");
+//        }
+        return result;
     }
-    
 };
 }
 
