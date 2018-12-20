9d800b6330fdd6b08e926470a4808feee26ff9db
Adrian Zwiener <adrian.zwiener@uni-tuebingen.de>
2018-12-20 17:08:11 +0100

    Remove uncommented code

--- src/node/muse_armcl_test_bag_node.cpp
+++ src/node/muse_armcl_test_bag_node.cpp
@@ -24,27 +24,7 @@ int main(int argc, char *argv[])
     muse_armcl::DataSet data_set;
     bool success = muse_armcl::DataSetLoader::loadFromBag(bag, topics[0], topics[1], data_set);
     std::cout << (success ? " loaded data successfully" : " loading data failed") << std::endl;
-//    std::size_t nmsg = view.size();
-//    std::size_t counter = 0;
-//    for(const rosbag::MessageInstance &m : view) {
-//        uint64_t nsecs = m.getTime().toNSec();
-//        if(m.getTopic() == topics[0]) {
-////            std::cout << "[" << topics[0] << "]:" << nsecs << std::endl;
-//            tf::tfMessage::Ptr tf = m.instantiate<tf::tfMessage>();
-//            if(tf)
-//                data_set.setTf(nsecs, *tf);
-//        }
-//        if(m.getTopic() == topics[1]){
-////            std::cout << "[" << topics[1] << "]:" <<  nsecs << std::endl;
-//            jaco2_contact_msgs::Jaco2CollisionSequence::Ptr sequence = m.instantiate<jaco2_contact_msgs::Jaco2CollisionSequence>();
-//            if(sequence)
-//                data_set.setContactData(nsecs, *sequence);
-//        }
-//        std::cout << static_cast<double>(++counter)/ static_cast<double>(nmsg) *100<< " % of msg read" << std::endl;
-//    }
 
-//    std::cout << view.size() << ", " << view.size() / 2 << ", " << view.size() % 2 << std::endl;
-//    std::cout << data_set.size() << ((data_set.size() == view.size() / 2) ? " check passed " : " check failed ") << std::endl;
 
     bag.close();
 
