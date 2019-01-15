6a151e7d64bdc4bb267df9206a091f11a609c7ba
Richard Hanten <richard.hanten@uni-tuebingen.de>
2019-01-15 12:20:19 +0100

    nn clustering

--- src/density/nearest_neighbor_density.cpp
+++ src/density/nearest_neighbor_density.cpp
@@ -4,7 +4,7 @@
 #include <cslibs_math/statistics/weighted_distribution.hpp>
 
 namespace muse_armcl {
-class /*EIGEN_ALIGN16*/ NearestNeighborDensity : public muse_armcl::SampleDensity
+class EIGEN_ALIGN16 NearestNeighborDensity : public muse_armcl::SampleDensity
 {
 public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
@@ -12,24 +12,33 @@ public:
     using vertex_t               = cslibs_mesh_map::MeshMap::VertexHandle;
     using distribution_t         = cslibs_math::statistics::WeightedDistribution<3>;
 
-    struct vertex_distribution_t /*EIGEN_ALIGN16*/ {
+    struct EIGEN_ALIGN16 vertex_distribution
+    {
         EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-        using allocator_t = Eigen::aligned_allocator<vertex_distribution_t>;
-        using Ptr = std::shared_ptr<vertex_distribution_t>;
+        using allocator_t = Eigen::aligned_allocator<vertex_distribution>;
+        using Ptr = std::shared_ptr<vertex_distribution>;
 
-        vertex_t        handle;
-        distribution_t  mean;
-        int             cluster_id = -1;
+        vertex_t                        handle;
+        distribution_t                  distribution;
+        int                             cluster_id = -1;
+        std::vector<sample_t const*>    samples;
     };
 
-    using vertex_distributions_t = std::unordered_map<std::size_t,
-                                                      std::unordered_map<int, vertex_distribution_t::Ptr>>;
+    struct EIGEN_ALIGN16 cluster_distribution
+    {
+        EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+        using allocator_t = Eigen::aligned_allocator<cluster_distribution>;
+        using Ptr = std::shared_ptr<cluster_distribution>;
 
-    using cluster_map_t  = std::unordered_map<int,
-    distribution_t,
-    std::hash<int>,
-    std::equal_to<int>,
-    Eigen::aligned_allocator<std::pair<const int, distribution_t>>>;
+        distribution_t                  distribution;
+        std::vector<sample_t const*>    samples;
+    };
+
+
+    using vertex_distributions_t = std::unordered_map<std::size_t,
+                                                      std::unordered_map<int, vertex_distribution::Ptr>>;
+
+    using cluster_map_t          = std::unordered_map<int, cluster_distribution::Ptr>;
 
 
     void setup(const map_provider_map_t &map_providers,
@@ -40,6 +49,7 @@ public:
         ignore_weight_ = nh.param(param_name("ignore_wieght"), false);
         radius_        = nh.param(param_name("radius"), 0.1);
         radius_ *= radius_;
+        relative_weight_threshold_ = nh.param(param_name("relative_weight_threshold"), 0.8);
 
         const std::string map_provider_id = nh.param<std::string>("map", ""); /// toplevel parameter
         if (map_provider_id == "")
@@ -59,13 +69,54 @@ public:
 
     void contacts(sample_vector_t &states) const override
     {
+        const muse_smc::StateSpace<StateSpaceDescription>::ConstPtr ss = map_provider_->getStateSpace();
+        if (!ss->isType<MeshMap>())
+            return;
 
+        using mesh_map_tree_t = cslibs_mesh_map::MeshMapTree;
+        const mesh_map_tree_t *map = ss->as<MeshMap>().data();
+
+        auto get_nearest = [&map](const cluster_distribution &c)
+        {
+            double min_distance = std::numeric_limits<double>::max();
+            sample_t const * sample = nullptr;
+            for(const sample_t* s : c.samples) {
+                const Eigen::Vector3d mean = c.distribution.getMean();
+                const Eigen::Vector3d pos =  s->state.getPosition(map->getNode(sample->state.map_id)->map);
+                const double distance = (mean - pos).squaredNorm();
+                if(distance < min_distance) {
+                    min_distance = distance;
+                    sample = s;
+                }
+            }
+            return sample;
+        };
+
+        double mean_weight = 0;
+        for(auto &c : clusters_) {
+            const cluster_distribution::Ptr &d = c.second;
+            mean_weight += d->distribution.getWeight();
+        }
+        mean_weight /= static_cast<double>(clusters_.size());
+
+        /// iterate the clusters and find the point nearest to the mean to estimate the contact
+        ///  mean cluster weights ...
+        for(auto &c : clusters_) {
+            /// drop a cluster if it is not weighted high enough compared to the others
+            if(c.second->distribution.getWeight() * relative_weight_threshold_ < mean_weight)
+                continue;
+
+            sample_t const * s = get_nearest(*c.second);
+            if(s != nullptr) {
+                states.emplace_back(*s);
+            }
+        }
     }
 
     void clear() override
     {
         vertex_distributions_.clear();
-        cluster_distributions_.clear();
+        clusters_.clear();
     }
 
     void insert(const sample_t &sample) override
@@ -76,9 +127,9 @@ public:
         const int id =  s < 0.5 ? acti_handle.idx() : goal_handle.idx();
         const std::size_t map_id = sample.state.map_id;
 
-        vertex_distribution_t::Ptr &d = vertex_distributions_[map_id][id];
+        vertex_distribution::Ptr &d = vertex_distributions_[map_id][id];
         if(!d) {
-            d.reset(new vertex_distribution_t);
+            d.reset(new vertex_distribution);
             d->handle = s < 0.5 ? acti_handle : goal_handle;
         }
         const muse_smc::StateSpace<StateSpaceDescription>::ConstPtr ss = map_provider_->getStateSpace();
@@ -88,7 +139,8 @@ public:
         using mesh_map_tree_t = cslibs_mesh_map::MeshMapTree;
         const mesh_map_tree_t *map = ss->as<MeshMap>().data();
         const cslibs_math_3d::Vector3d pos = sample.state.getPosition(map->getNode(sample.state.map_id)->map);
-        d->mean.add(pos.data(), ignore_weight_ ? 1.0 : sample.weight);
+        d->distribution.add(pos.data(), ignore_weight_ ? 1.0 : sample.weight);
+        d->samples.emplace_back(&sample);
     }
 
     void estimate() override
@@ -104,7 +156,7 @@ public:
         /// clustering by map
         for(auto &vd : vertex_distributions_) {
             for(auto &v : vd.second) {
-                vertex_distribution_t::Ptr &d = v.second;
+                vertex_distribution::Ptr &d = v.second;
 
                 if(d->cluster_id != -1)
                     continue;
@@ -112,7 +164,7 @@ public:
                 /// check all neighbors for cluster id
                 /// if d < dmax : assign to neighbor
                 /// else new cluster
-                const Eigen::Vector3d mean = d->mean.getMean();
+                const Eigen::Vector3d mean = d->distribution.getMean();
                 const cslibs_mesh_map::MeshMapTreeNode* state_map = map->getNode(vd.first);
                 const auto& neighbors = state_map->map.getNeighbors(d->handle);
                 double min_dist = std::numeric_limits<double>::max();
@@ -131,12 +183,19 @@ public:
 
                 if(min_id != -1){
                     d->cluster_id = min_id;
-                    distribution_t &md = cluster_distributions_[min_id];
-                    md += d->mean;
+                    cluster_distribution::Ptr &cd = clusters_[min_id];
+                    assert(cd);
+                    cd->samples.insert(cd->samples.end(), d->samples.begin(), d->samples.end());
+                    cd->distribution += d->distribution;
                 } else {
                     ++cluster_id;
                     d->cluster_id = cluster_id;
-                    cluster_distributions_[cluster_id] = d->mean;
+                    cluster_distribution::Ptr cd = clusters_[cluster_id];
+                    if(!cd) {
+                        cd.reset(new cluster_distribution);
+                    }
+                    cd->distribution = d->distribution;
+                    cd->samples = d->samples;
                 }
             }
         }
@@ -144,11 +203,15 @@ public:
 
 private:
     vertex_distributions_t      vertex_distributions_;
-    cluster_map_t               cluster_distributions_;
+    cluster_map_t               clusters_;
     MeshMapProvider::Ptr        map_provider_;
     bool                        ignore_weight_;
     double                      radius_;
+    double                      relative_weight_threshold_;
 
 };
 }
+
+#include <class_loader/class_loader_register_macro.h>
+CLASS_LOADER_REGISTER_CLASS(muse_armcl::NearestNeighborDensity, muse_armcl::SampleDensity)
 
