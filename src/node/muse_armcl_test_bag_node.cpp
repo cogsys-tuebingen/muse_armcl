e626727e96b892e6d710391e9ab5a58afe19e3b1
Adrian Zwiener <adrian.zwiener@uni-tuebingen.de>
2019-01-21 13:35:51 +0100

    Add minor update

--- src/node/muse_armcl_test_bag_node.cpp
+++ src/node/muse_armcl_test_bag_node.cpp
@@ -24,7 +24,13 @@ int main(int argc, char *argv[])
     muse_armcl::DataSet data_set;
     bool success = muse_armcl::DataSetLoader::loadFromBag(bag, topics[0], topics[1], data_set);
     std::cout << (success ? " loaded data successfully" : " loading data failed") << std::endl;
-
+    std::size_t nd = 0;
+    for(auto val : data_set){
+        nd += val.data.size();
+    }
+    std::cout << "Data set contains: "
+              << data_set.size() << " sequences and "
+              << nd << " messages. " << std::endl;
 
     bag.close();
 
